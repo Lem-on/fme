917d8b94540106066ed728a668f115ad03bb0ebe
Lem-on <petr.smoldas@centrum.cz>
2015-08-22 17:38:03 +0200

    Update oznacovac.js

--- oznacovac.js
+++ oznacovac.js
@@ -1,20 +1,16 @@
-API.chatLog(":ballot_box_with_check: Označovač 3000 - NAČTEN!");
-
-API.off(API.CHAT, lemon);
-
-API.on(API.CHAT, lemon);
+API.chatLog("Označovač 3000");
+API.on(API.CHAT, lol); /* lemon */
  
- /* lemonjesuper */
  
-function lemon(love){
-	var msgData = love;
-	if(msgData.message.indexOf("!paint") > -1){
-
-		if(msgData.un == "Lem‏on" || msgData.un == "Cook‏ie"){
-			var narcisekjekrasavec = msgData.message.replace("označ", "");
-			nakresli(narcisekjekrasavec.toUpperCase());
-		} else {
-			API.sendChat("@"+msgData.un+" Hell Yeah! :P");
-	 } 
-	}
+/* lemon */
+ 
+function lol(a) {
+    var me = API.getUser().id;
+    var msg = a.message;
+    var from = a.uid;
+    if (me != from) {
+        if (msg.toLowerCase().indexOf("označ") > -1) {
+            API.sendChat("@•TiGeR• cz ♪♫");
+        }
+    }
 }
