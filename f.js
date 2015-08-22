23b25b1ac29d4e93ba170fa5493886e5a10fb6c7
Lem-on <petr.smoldas@centrum.cz>
2015-08-22 15:55:22 +0200

    Create f.js

---
+++ f.js
@@ -0,0 +1,16 @@
+API.chatLog("Nebuď sprostý nebo tě kopnu! [LOADED]");
+API.on(API.CHAT, lol); /* lemon */
+ 
+ 
+/* lemon */
+ 
+function lol(a) {
+    var me = API.getUser().id;
+    var msg = a.message;
+    var from = a.uid;
+    if (me != from) {
+        if (msg.toLowerCase().indexOf("píča") > -1 || msg.toLowerCase().indexOf("píče") > -1 || msg.toLowerCase().indexOf("píčo") >  -1 || msg.toLowerCase().indexOf("píču") > -1 || msg.toLowerCase().indexOf("kokot") > -1 || msg.toLowerCase().indexOf("kurva") > -1 || msg.toLowerCase().indexOf("čubka") > -1 || msg.toLowerCase().indexOf("čurák") > -1 || msg.toLowerCase().indexOf("děvka") > -1 || msg.toLowerCase().indexOf("koňomrd") > -1 || msg.toLowerCase().indexOf("kretén") > -1 || msg.toLowerCase().indexOf("zasraný") > -1 || msg.toLowerCase().indexOf("mrdat") > -1 || msg.toLowerCase().indexOf("šukat") > -1 || msg.toLowerCase().indexOf("zmrd") > -1 ) {
+            API.sendChat("@"+msgData.un+" Nebuď sprostý nebo tě kopnu!");
+        }
+    }
+}
