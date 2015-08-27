bdac743156d6c583f2fbea5a5892ea178ed70532
Lem-on <petr.smoldas@centrum.cz>
2015-08-27 20:21:07 +0200

    Update f.js

--- f.js
+++ f.js
@@ -10,6 +10,7 @@ function lol(a) {
     var from = a.uid;
     if (me != from) {
         if (msg.toLowerCase().indexOf("koza") > -1) {
+        if (msg.toLowerCase().indexOf("perník") > -1) {
             API.sendChat("@"+a.un+" Nebuď sprostý nebo tě kopnu!");
         }
     }
