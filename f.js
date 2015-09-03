47b7547dcbed565c75e06c7e20dc5eea6161e1a7
Lem-on <petr.smoldas@centrum.cz>
2015-09-03 15:52:09 +0200

    Update f.js

--- f.js
+++ f.js
@@ -9,6 +9,84 @@ function lol(a) {
     var msg = a.message;
     var from = a.uid;
     if (me != from) {
+        if (msg.toLowerCase().indexOf("čurák") > -1) {
+            API.sendChat("@"+a.un+" Nebuď sprostý nebo tě kopnu!");
+        }
+        if (msg.toLowerCase().indexOf("debil") > -1) {
+            API.sendChat("@"+a.un+" Nebuď sprostý nebo tě kopnu!");
+        }
+        if (msg.toLowerCase().indexOf("kokot") > -1) {
+            API.sendChat("@"+a.un+" Nebuď sprostý nebo tě kopnu!");
+        }
+        if (msg.toLowerCase().indexOf("kretén") > -1) {
+            API.sendChat("@"+a.un+" Nebuď sprostý nebo tě kopnu!");
+        }
+        if (msg.toLowerCase().indexOf("kurva") > -1) {
+            API.sendChat("@"+a.un+" Nebuď sprostý nebo tě kopnu!");
+        }
+        if (msg.toLowerCase().indexOf("píča") > -1) {
+            API.sendChat("@"+a.un+" Nebuď sprostý nebo tě kopnu!");
+        }
+        if (msg.toLowerCase().indexOf("píču") > -1) {
+            API.sendChat("@"+a.un+" Nebuď sprostý nebo tě kopnu!");
+        }
+        if (msg.toLowerCase().indexOf("kurv") > -1) {
+            API.sendChat("@"+a.un+" Nebuď sprostý nebo tě kopnu!");
+        }
+        if (msg.toLowerCase().indexOf("koňomrd") > -1) {
+            API.sendChat("@"+a.un+" Nebuď sprostý nebo tě kopnu!");
+        }
+        if (msg.toLowerCase().indexOf("buzerant") > -1) {
+            API.sendChat("@"+a.un+" Nebuď sprostý nebo tě kopnu!");
+        }
+        if (msg.toLowerCase().indexOf("buzna") > -1) {
+            API.sendChat("@"+a.un+" Nebuď sprostý nebo tě kopnu!");
+        }
+        if (msg.toLowerCase().indexOf("idiot") > -1) {
+            API.sendChat("@"+a.un+" Nebuď sprostý nebo tě kopnu!");
+        }
+        if (msg.toLowerCase().indexOf("kunda") > -1) {
+            API.sendChat("@"+a.un+" Nebuď sprostý nebo tě kopnu! (i přes to, že tohle slovo používá Zeman :joy:)");
+        }
+        if (msg.toLowerCase().indexOf("srač") > -1) {
+            API.sendChat("@"+a.un+" Nebuď sprostý nebo tě kopnu!");
+        }
+        if (msg.toLowerCase().indexOf("sráč") > -1) {
+            API.sendChat("@"+a.un+" Nebuď sprostý nebo tě kopnu!");
+        }
+        if (msg.toLowerCase().indexOf("vůl") > -1) {
+            API.sendChat("@"+a.un+" Nebuď sprostý nebo tě kopnu! (bůůů)");
+        }
+        if (msg.toLowerCase().indexOf("zmrd") > -1) {
+            API.sendChat("@"+a.un+" Nebuď sprostý nebo tě kopnu!");
+        }
+        if (msg.toLowerCase().indexOf("demen") > -1) {
+            API.sendChat("@"+a.un+" Nebuď sprostý nebo tě kopnu!");
+        }
+        if (msg.toLowerCase().indexOf("jebat") > -1) {
+            API.sendChat("@"+a.un+" Nebuď sprostý nebo tě kopnu!");
+        }
+        if (msg.toLowerCase().indexOf("šukat") > -1) {
+            API.sendChat("@"+a.un+" Nebuď sprostý nebo tě kopnu!");
+        }
+        if (msg.toLowerCase().indexOf("mrdat") > -1) {
+            API.sendChat("@"+a.un+" Nebuď sprostý nebo tě kopnu!");
+        }
+        if (msg.toLowerCase().indexOf("zmrd") > -1) {
+            API.sendChat("@"+a.un+" Nebuď sprostý nebo tě kopnu!");
+        }
+        if (msg.toLowerCase().indexOf("sraný") > -1) {
+            API.sendChat("@"+a.un+" Nebuď sprostý nebo tě kopnu!");
+        }
+        if (msg.toLowerCase().indexOf("srany") > -1) {
+            API.sendChat("@"+a.un+" Nebuď sprostý nebo tě kopnu!");
+        }
+        if (msg.toLowerCase().indexOf("píči") > -1) {
+            API.sendChat("@"+a.un+" Nebuď sprostý nebo tě kopnu!");
+        }
+        if (msg.toLowerCase().indexOf("srač") > -1) {
+            API.sendChat("@"+a.un+" Nebuď sprostý nebo tě kopnu!");
+        }
         if (msg.toLowerCase().indexOf("skip") > -1) {
             API.sendChat("@"+a.un+" Pokud tento song neporušuje pravidla tak ho prostě nepřeskočím! :/");
         }
